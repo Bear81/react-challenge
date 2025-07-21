a9bebd9a7bd6cd358342fcfa0737963d3025ed58
Bear81 <142313344+Bear81@users.noreply.github.com>
2025-07-21 14:10:18 +0100

    Create NavBarForm Component

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import css from './css/Sidebar.module.css';
+
+const Sidebar = () => <div className = {css.sidebar}>
+  <a href = "#." target= "_blank">My Photos</a>
+  <a href = "#." target= "_blank">My Illustrations</a>
+  <a href = "#." target= "_blank">My Paintings</a>
+</div>;
+
+export default Sidebar;
---
+++ my-app/src/components/es7Snippits.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function () {
+  return (
+    <div></div>
+  )
+}
+
+export default 
+
+import React from 'react'
+
+const es7Snippits = () => {
+  return (
+    <div>es7Snippits</div>
+  )
+}
+
+export default es7Snippits
